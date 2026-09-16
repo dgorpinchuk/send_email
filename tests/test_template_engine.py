3c2224720f47930ca66a57136b3e289baa2b15c6
Gorpinchuk Dmitry <62282859+dgorpinchuk@users.noreply.github.com>
2026-09-16 13:31:45 +0300

    Test default unsubscribe_link

--- tests/test_template_engine.py
+++ tests/test_template_engine.py
@@ -11,3 +11,18 @@ def test_render_escapes_html():
 
 def test_missing_values():
     assert validate_columns("{{ name }} {{ company }}", ["email", "name"]) == ["company"]
+
+
+def test_default_unsubscribe_link():
+    template = '<a href="{{ unsubscribe_link }}">Unsubscribe</a>'
+    expected = '<a href="https://mail.yandex.ru/unsubscribe.html">Unsubscribe</a>'
+    assert render(template, {}) == expected
+    assert validate_columns(template, ["email", "name"]) == []
+
+
+def test_explicit_unsubscribe_link_overrides_default():
+    template = '<a href="{{ unsubscribe_link }}">Unsubscribe</a>'
+    custom = "https://example.com/unsubscribe/123"
+    assert render(template, {"unsubscribe_link": custom}) == (
+        f'<a href="{custom}">Unsubscribe</a>'
+    )
